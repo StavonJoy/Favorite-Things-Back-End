e060148e249013c150ffcda764edb88f995cbc2f
Cory Spicer <corys4139@gmail.com>
2020-09-17 15:56:51 -0400

    auth

--- routes/lists.js
+++ routes/lists.js
@@ -1,11 +1,18 @@
 const router = require('express').Router();
-// const List = require('../models/List')
 const listsCtrl = require('../controllers/api/lists')
 
 router.get('/', listsCtrl.index);
-router.post('/', listsCtrl.create);
-router.get('/:id', listsCtrl.show);
-router.put('/:id', listsCtrl.update);
-router.delete('/:id', listsCtrl.delete);
+
+router.use(require('../config/auth'))
+router.post('/', checkAuth, listsCtrl.create);
+router.get('/:id', checkAuth, listsCtrl.show);
+router.put('/:id', checkAuth, listsCtrl.update);
+router.delete('/:id', checkAuth, listsCtrl.delete);
+
+function checkAuth(req, res, next) {
+    console.log(req.user)
+    if (req.user) return next();
+    return res.status(401).json({msg: 'Not Authorized'});
+}
 
 module.exports = router;
